16ff9e0d03b9d6f250c5a0b29cce862ac01246ff
singh_shub <singh_shub@GUUP-71623-WL.India.XoriantCorp.com>
2022-09-13 17:02:41 +0530

    TEst

---
+++ MyGitProject/src/test/java/SeleniumTest/MyGitProject/AppTest.java
@@ -0,0 +1,13 @@
+package SeleniumTest.MyGitProject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
